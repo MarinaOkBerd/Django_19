d368575cd1034cb4fa046617134369ab68e212cd
Marina Berdnikova <berdni4ka@rambler.ru>
2023-05-17 11:22:01 +0300

    Add homework 20.1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=150, verbose_name='категория')
+    price = models.FloatField(verbose_name='цена')#ttttoooooooooooooooooooddddddddddddddoooooooooooooo
+    date_of_creation = models.DateField(verbose_name='дата создания')
+    last_modified_date = models.DateField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.price} {self.category}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('category',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
